c170a15f0ba249350e73b50b68d38f068462e7b7
mbroughton93 <mbroughton0910@students.mc3.edu>
2024-11-04 19:58:27 -0500

    Updating Code with Images and Code

--- src/WeatherAppGUI.java
+++ src/WeatherAppGUI.java
@@ -1,4 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherAppGUI extends JFrame {
     public WeatherAppGUI() {
@@ -16,13 +21,50 @@ public class WeatherAppGUI extends JFrame {
         setResizable(false);
 
         addGuiComponents();
+
+
     }
 
-
     private void addGuiComponents() {
+
         JTextField searchTextField = new JTextField();
 
         searchTextField.setBounds(15, 15, 351, 45);
+
         add(searchTextField);
+
+        searchTextField.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        JButton searchButton = new JButton(loadImage("src/Assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        JLabel weatherConditionImage = new JLabel(loadImage("src/Assets/cloudy.png"));
+        weatherConditionImage.setBounds(0,125,450,217);
+        add(weatherConditionImage);
+
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 54);
+        temperatureText.setFont(new Font("Arial", Font.PLAIN, 40));
+
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+    }
+
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            return new ImageIcon(image);
+        }catch(IOException e) {
+            e.printStackTrace();
+
+
+        }
+
+        System.out.println("Could not find resource");
+        return null;
     }
 }
